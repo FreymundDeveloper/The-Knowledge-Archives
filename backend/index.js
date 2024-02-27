f4cf9b0215749ab70a426646503d45ec1c1ab7e1
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-02-27 00:32:24 -0300

    Saving Structure

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const app = require('express')();
 const consign = require('consign');
 
-consign().then('./config/middlewares.js').into(app);
+consign().then('./config/middlewares.js')
+         .then('./api')
+         .then('./config/routes.js')
+         .into(app);
 
 app.listen(3001, () => {
     console.log('Ah sh*t, here we go again')
