98e9c990c79571ef70bd80d03d09b30d8b53e190
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-02-29 17:37:08 -0300

    Statistics Integrated with MongoDB

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/knowledge_stats', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/knowledge_stats', { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(err => {
         const message = 'Error, MongoDB not Connected!!';
         console.log('\x1b[41m%s\x1b[37m', message, '\x1b[0m');
