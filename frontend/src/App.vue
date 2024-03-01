4e910672aa12086a449b33c811978c05674b5334
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-01 18:07:22 -0300

    Started Templates

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
 	<div id="app">
-		<h1>Start Version</h1>
+		<HeaderMain />
+		<MenuMain />
+		<ContentMain />
+		<FooterMain />
 	</div>
 </template>
 
 <script>
+import HeaderMain from './components/template/HeaderMain.vue';
+import MenuMain from './components/template/MenuMain.vue';
+import ContentMain from './components/template/ContentMain.vue';
+import FooterMain from './components/template/FooterMain.vue';
+
 export default {
 	name: "App",
+	components: { HeaderMain, MenuMain, ContentMain, FooterMain }
 }
 </script>
 
 <style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
 
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas: 
+			"header header"
+			"menu content"
+			"menu footer";
+	}
 </style>
---
+++ frontend/src/components/template/FooterMain.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <span>Copyright <strong>FD</strong> © 2024</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterMain'
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
