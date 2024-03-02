4ed165190e42c4cdaf4e5016f830d3755c41721a
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-01 22:05:42 -0300

    Title Component Model

---
+++ frontend/src/components/template/TitleMain.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="title-main">
+        <h1><i v-if="icon" :class="icon"></i>{{ main }}</h1>
+        <h2>{{ sub }}</h2>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitleMain',
+    props: ['icons', 'main', 'sub']
+}
+</script>
+
+<style>
+    .title-main h1 {
+        margin: 0px;
+    }
+
+    .title-main h2 {
+        color: #777;
+        margin-top: 5px;
+        font-size: 1.3em;
+    }
+</style>
