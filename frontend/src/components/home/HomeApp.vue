e78f47747341bfbd62e530c9775c93f46cffed27
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-04 19:47:35 -0300

    Home Component

--- frontend/src/components/home/HomeApp.vue
+++ frontend/src/components/home/HomeApp.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
         <TitleMain icon="fa fa-home" main=" Dashboard" sub="The Knowledge Archives" />
+        <div class="stats">
+          <StatApp title="Categories" :value="stat.categories" icon="fa fa-folder" color="#d54d50" />
+          <StatApp title="Articles" :value="stat.articles" icon="fa fa-file" color="#3bc480" />
+          <StatApp title="Users" :value="stat.users" icon="fa fa-user" color="#3282cd" />
+        </div>
   </div>
 </template>
 
 <script>
 import TitleMain from '../template/TitleMain.vue'
+import StatApp from './StatApp.vue'
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
 
 export default {
     name: 'HomeApp',
-    components: { TitleMain }
+    components: { TitleMain, StatApp },
+    data: function() {
+      return {
+        stat: {}
+      }
+    },
+    methods: {
+      getStats() {
+        axios.get(`${baseApiUrl}/stats`).then(response => this.stat = response.data)
+      }
+    },
+    mounted() {
+      this.getStats()
+    }
 }
 </script>
 
 <style>
-
+  .stats {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,11 @@ import './config/bootstrap'
 import store from './config/store'
 import router from './config/router'
 
+import axios from 'axios'
+
 Vue.config.productionTip = false
+
+axios.defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6Ilh5IEluYWciLCJlbWFpbCI6Inh5aWdAZ21haWwuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE3MDk1ODc2ODQsImV4cCI6MTcwOTg0Njg4NH0.VkD04vfhWVIYnpiJy1xJ39Dw_A0dmMhYVugaDlwqSrc'
 
 new Vue({
   store,
