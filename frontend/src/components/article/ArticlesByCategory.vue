a73bd30377af1f015971fa573d855db85a61015a
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-06 01:23:58 -0300

    ArticlesByCategory Loading More Button

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="articles-by-category">
         <TitleMain icon="fa fa-folder-o" :main="category.name" sub="Category" />
+        <ul>
+            <li v-for="article in articles" :key="article.id">
+                {{ article.name }}
+            </li>
+        </ul>
+        <div class="load-more">
+            <button class="btn btn-lg btn-outline-primary" 
+                v-if="loadMore" @click="getArticles">Load more articles</button>
+        </div>
     </div>
 </template>
 
@@ -24,15 +33,34 @@ export default {
         getCategory() {
             const url = `${baseApiUrl}/categories/${this.category.id}`
             axios(url).then(res => this.category = res.data)
+        },
+        getArticles() {
+            const url = `${baseApiUrl}/categories/${this.category.id}/articles?page=${this.page}`
+            axios(url).then(res => {
+                this.articles = this.articles.concat(res.data)
+                this.page++
+
+                if(res.data.length === 0) this.loadMore = false
+            })
         }
     },
     mounted() {
         this.category.id = this.$route.params.id
         this.getCategory()
+        this.getArticles()
     }
 }
 </script>
 
 <style>
+    .articles-by-category ul {
+        list-style-type: none;
+        padding: 0px;
+    }
 
+    .articles-by-category .load-more {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 </style>
