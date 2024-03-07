125b273b262c207e3f2fa2e7abfe4c4f085abcef
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-07 17:19:42 -0300

    Token Management and Validation System

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -4,7 +4,7 @@ const admin = require('./admin');
 module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
-    app.post('/validateToen', app.api.auth.validateToken)
+    app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
         .all(app.config.passport.authenticate())
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,22 +2,57 @@
 	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
 		<HeaderMain title="The Knowledge Archives" :hideToggle="!user" :hideUserDropdown="!user" />
 		<MenuMain v-if="user"/>
-		<ContentMain />
+		<LoadingMain v-if="validatingToken" />
+		<ContentMain v-else />
 		<FooterMain />
 	</div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import axios from 'axios'
+import { baseApiUrl, userKey } from '@/global'
 import HeaderMain from './components/template/HeaderMain.vue';
 import MenuMain from './components/template/MenuMain.vue';
 import ContentMain from './components/template/ContentMain.vue';
 import FooterMain from './components/template/FooterMain.vue';
+import LoadingMain from './components/template/LoadingMain.vue';
 
+// cast.ru@gmail.com
 export default {
 	name: "App",
-	components: { HeaderMain, MenuMain, ContentMain, FooterMain },
-	computed: mapState(['isMenuVisible', 'user'])
+	components: { HeaderMain, MenuMain, ContentMain, FooterMain, LoadingMain },
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return { validatingToken: true }
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if (!userData) {
+				this.validatingToken = false
+				return this.$router.push({ name: 'auth' })
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if (res.data) this.$store.commit('setUser', userData)
+			else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validatingToken = false
+		}
+	},
+	created() {
+		this.validateToken()
+	}
 }
 </script>
 
